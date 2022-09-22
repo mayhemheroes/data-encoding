467991095d69f04f31f766952ee90eff8b9bf041
xansec <76011430+xansec@users.noreply.github.com>
2022-09-22 00:57:33 -0400

    Update Dockerfile

--- lib/fuzz/examples/debug.rs
+++ lib/fuzz/examples/debug.rs
@@ -1,7 +1,15 @@
-use data_encoding_fuzz::cmd;
+extern crate data_encoding_fuzz;
+
+use std::io::Read;
+
+use data_encoding_fuzz::generate_specification;
 
 fn main() {
-    let path = cmd::path(true);
-    let input = std::fs::read(&path).unwrap();
-    cmd::execute(&cmd::target(&path), &input);
+    let stdin = std::io::stdin();
+    let mut input = Vec::new();
+    stdin.lock().read_to_end(&mut input).unwrap();
+    let mut data = &input[..];
+    println!("{:#?}", generate_specification(&mut data));
+    println!("spec = {:?}", &input[.. input.len() - data.len()]);
+    println!("data = {:?}", data);
 }
